d14375c0f26b9bf3d5600e07dc5c7bc9e5d82248
max razhnov <shahteryshka@gmail.com>
2019-05-16 12:31:05 +0300

    feat: add lab3

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -8,10 +8,10 @@ const BTN_YUV = document.getElementById("btn-YUV");
 const BTN_RGB = document.getElementById("btn-RGB");
 
 // transformation buttons
-const BTN_TRANSLATION = document.getElementById("btn-translation");
-const BTN_ROTATE = document.getElementById("btn-rotate");
-const BTN_SCALE = document.getElementById("btn-scale");
-const BTN_SHIFT = document.getElementById("btn-shift");
+// const BTN_TRANSLATION = document.getElementById("btn-translation");
+// const BTN_ROTATE = document.getElementById("btn-rotate");
+// const BTN_SCALE = document.getElementById("btn-scale");
+// const BTN_SHIFT = document.getElementById("btn-shift");
 
 //user buttons
 const BTN_UPLOAD_FILE = document.getElementsByClassName("item-upload")[0];
@@ -44,7 +44,6 @@ imgElement.onload = () => {
   if (str === 'jpg' || str === 'png' || str === 'gif' || str === 'tiff') {
     cv.imshow("outputCanvas_SRC_IMG", src);
     FileName = fileInput.value;
-    console.log(src.type())
   } else {
     alert("Нужно загрузить изображение");
   }
@@ -52,31 +51,70 @@ imgElement.onload = () => {
 
 BTN_SAVE_FILE.removeEventListener("click", saveDefault)
 BTN_RGB.removeEventListener("click", rgbDisplay);
-BTN_CMY.removeEventListener("click", cmyDisplay);
-BTN_GRAY.removeEventListener("click", grayDisplay);
-BTN_HSV.removeEventListener("click", hsvDisplay);
-BTN_HSL.removeEventListener("click", hslDisplay);
-BTN_YUV.removeEventListener("click", yuvDisplay);
+// BTN_CMY.removeEventListener("click", cmyDisplay);
+// BTN_GRAY.removeEventListener("click", grayDisplay);
+// BTN_HSV.removeEventListener("click", hsvDisplay);
+// BTN_HSL.removeEventListener("click", hslDisplay);
+// BTN_YUV.removeEventListener("click", yuvDisplay);
 BTN_CHANNEL_1.removeEventListener("click", saveChannel_1);
 BTN_CHANNEL_2.removeEventListener("click", saveChannel_2);
 BTN_CHANNEL_3.removeEventListener("click", saveChannel_3);
+
+
+const BTN_TRANS1 = document.getElementById("btn-prep");
+const BTN_TRANS2 = document.getElementById("btn-lostLayout");
+const BTN_TRANS3 = document.getElementById("btn-saveLayout");
+const BTN_TRANS4 = document.getElementById("btn-trans4");
+const BTN_TRANS5 = document.getElementById("btn-trans5");
+const BTN_TRANS6 = document.getElementById("btn-trans6");
+const BTN_TRANS7 = document.getElementById("btn-trans7");
+const BTN_TRANS8 = document.getElementById("btn-trans8");
+const BTN_TRANS9 = document.getElementById("btn-trans9");
+const BTN_TRANS10 = document.getElementById("btn-trans10");
+
+
+BTN_TRANS1.removeEventListener("click", trans1);
+BTN_TRANS2.removeEventListener("click", trans2);
+BTN_TRANS3.removeEventListener("click", trans3);
+BTN_TRANS4.removeEventListener("click", trans4);
+BTN_TRANS5.removeEventListener("click", trans5);
+BTN_TRANS6.removeEventListener("click", trans6);
+BTN_TRANS7.removeEventListener("click", trans7);
+BTN_TRANS8.removeEventListener("click", trans8);
+BTN_TRANS9.removeEventListener("click", trans9);
+BTN_TRANS10.removeEventListener("click", trans10);
+
 
 // BTN_TRANSLATION.removeEventListener("click",);
 // BTN_ROTATE.removeEventListener("click");
 // BTN_SCALE.removeEventListener("click");
 // BTN_SHIFT.removeEventListener("click");
 
+BTN_TRANS1.addEventListener("click", trans1);
+BTN_TRANS2.addEventListener("click", trans2);
+BTN_TRANS3.addEventListener("click", trans3);
+BTN_TRANS4.addEventListener("click", trans4);
+BTN_TRANS5.addEventListener("click", trans5)
+BTN_TRANS6.addEventListener("click", trans6)
+BTN_TRANS7.addEventListener("click", trans7)
+BTN_TRANS8.addEventListener("click", trans8)
+BTN_TRANS9.addEventListener("click", trans9);
+BTN_TRANS10.addEventListener("click", trans10);
+
+
 BTN_SAVE_FILE.addEventListener("click", saveDefault);
 BTN_RGB.addEventListener("click", rgbDisplay);
-BTN_CMY.addEventListener("click", cmyDisplay);
-BTN_GRAY.addEventListener("click", grayDisplay);
-BTN_HSV.addEventListener("click", hsvDisplay);
-BTN_HSL.addEventListener("click", hslDisplay);
-BTN_YUV.addEventListener("click", yuvDisplay);
+// BTN_CMY.addEventListener("click", cmyDisplay);
+// BTN_GRAY.addEventListener("click", grayDisplay);
+// BTN_HSV.addEventListener("click", hsvDisplay);
+// BTN_HSL.addEventListener("click", hslDisplay);
+// BTN_YUV.addEventListener("click", yuvDisplay);
+
 
 BTN_CHANNEL_1.addEventListener("click", saveChannel_1);
 BTN_CHANNEL_2.addEventListener("click", saveChannel_2);
 BTN_CHANNEL_3.addEventListener("click", saveChannel_3);
+
 
 function saveChannel_1() {
   let canvas = document.getElementById("outputCanvasChannel_1");
@@ -309,7 +347,7 @@ function yellowChannel(imgYellow) {
   return imgYellow;
 }
 
-BTN_SCALE.addEventListener("click", scale);
+// BTN_SCALE.addEventListener("click", scale);
 
 function scale() {
   if (src === null) {
@@ -319,4 +357,194 @@ function scale() {
     mat.put(0, 0, 2, 0, 10, 0, 2, 10);
     // cv.warpAffine(dst, dst,);
   }
+}
+
+
+function trans1() {
+  let img = src.clone();
+  let value = +prompt('chislo', 127);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] > value) {
+      img.data[i] = 255;
+    } else {
+      img.data[i] = 0;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans2() {
+  let minValue = +prompt('vvedite min interval', 75);
+  let maxValue = +prompt('vvedite max interval', 150);
+  let img = src.clone();
+  // let count = 0;
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] > minValue && img.data[i] < maxValue) {
+      img.data[i] = 255;
+    } else {
+      img.data[i] = 0;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans3() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 180);
+  let maxValue = +prompt('vvedite max interval', 240);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  // console.log(img.data)
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] > minValue && img.data[i] < maxValue) {
+      // img.data[i] = 255;
+      img.data[i] = 255;
+      // } else {
+      // img.data[i] = alpha * (img.data[i]) / (255);
+      // }
+    }
+    // console.log(img.data)
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans4() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  // let maxValue = +prompt('vvedite max interval', 240);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] > minValue) {
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+      img.data[i] = 255;
+    }
+    // } else if (img.data[i] < minValue) {
+    //   img.data[i] = 0;
+    // } else if (img.data[i] > maxValue) {
+    //   img.data[i] = 255;
+    // }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans5() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] < minValue) {
+      img.data[i] = 0;
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+    } else if (img.data[i] > maxValue) {
+      img.data[i] = 255;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans6() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] < minValue) {
+      img.data[i] = 255;
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+    } else if (img.data[i] > maxValue) {
+      img.data[i] = 0;
+    } else {
+      img.data[i] = 255 - img.data[i];
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans7() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] < minValue) {
+      img.data[i] = 0;
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+    } else if (img.data[i] > maxValue) {
+      img.data[i] = 0;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans8() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] < minValue) {
+      img.data[i] = 255;
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+    } else if (img.data[i] > maxValue) {
+      img.data[i] = 255;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans9() {
+  let img = src.clone();
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    if (img.data[i] < minValue) {
+      img.data[i] = 127;
+      // img.data[i] = alpha * (img.data[i] - 0) / (255 - 0);
+    } else if (img.data[i] > maxValue) {
+      img.data[i] = 127;
+    }
+  }
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+
+function trans10() {
+  let img = src.clone();
+  // let alpha = 255;
+  let minValue = +prompt('vvedite min interval', 100);
+  let maxValue = +prompt('vvedite max interval', 200);
+  let n = +prompt('vvedite kol-vo pil ', 3);
+  let dl = +Math.floor(255 / n);
+  cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY, 0);
+  for (let i = 0; i < img.data.length; i++) {
+    img.data[i] = ((img.data[i] - (dl) * Math.floor(img.data[i] / dl)) * 255 / Math.floor(255 / n));
+  }
+  // console.log(img.data)
+  cv.imshow("outputCanvas_DST_IMG", img);
+}
+
+const BTN_RESET = document.getElementById("btn-reset");
+BTN_RESET.addEventListener("click", reset);
+
+function reset() {
+  const canvas = document.getElementById("outputCanvas_DST_IMG");
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
 }
